d4c3e6277411b02758ea65a24aa9f3be16a5574d
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-21 02:21:40 +0100

    Random daily selection (helper function)

---
+++ Helpers/DeterministicSeeding.ts
@@ -0,0 +1,35 @@
+import { Chance } from "chance";
+
+/**
+ * Gets a number guaranteed to be the same throughout today, and guaranteed to change every day.
+ * @returns Seed value.
+ */
+function todaySeed(): number {
+  return Number(
+    new Date()
+      .toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
+      .replace(/[^\d]/g, "")
+  );
+}
+
+/**
+ * Gets a number of items from an array, guaranteed to be deterministic based on the seed.
+ * @param seed Seed of the returned items.
+ * @param numItems Number of items to return.
+ * @param array Array of possible items.
+ * @returns Deterministic items from array as per the seed.
+ */
+export function getDeterministicArrayItems<T>(
+  numItems: number,
+  array: T[]
+): T[] {
+  const seedValue = todaySeed();
+
+  const chance = new Chance(seedValue);
+
+  return chance.shuffle(array.slice()).slice(0, numItems);
+}
