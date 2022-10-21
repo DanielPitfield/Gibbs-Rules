20dce2fda1fd0763e15d3f01c3087e23a169a429
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-21 03:30:53 +0100

    Gibbs rules (Data)

--- Data/GibbsRules.ts
+++ Data/GibbsRules.ts
@@ -1,4 +1,47 @@
 // TODO: Add the episode(s) in which the rule was said?
 export type GibbsRule = { number: number; rule: string };
 
-export const gibbsRules: GibbsRule[] = [];
+export const gibbsRules: GibbsRule[] = [
+  { number: 1, rule: "Never let suspects sit together." },
+  { number: 1, rule: "Never screw over your partner." },
+  { number: 2, rule: "Always wear gloves at a crime scene." },
+  { number: 3, rule: "Never believe what you are told. Double-check." },
+  { number: 3, rule: "Never be unreachable." },
+  {
+    number: 4,
+    rule: "Best way to keep a secret: Keep it to yourself. Second best: Tell one other person—if you must. There is no third best.",
+  },
+  { number: 5, rule: "You don’t waste good." },
+  { number: 6, rule: "Never say you’re sorry." },
+  { number: 7, rule: "Always be specific when you lie." },
+  { number: 8, rule: "Never take anything for granted." },
+  { number: 9, rule: "Never go anywhere without a knife." },
+  { number: 10, rule: "Never get involved personally on a case." },
+  { number: 11, rule: "When the job is done, walk away." },
+  { number: 12, rule: "Never date a coworker." },
+  { number: 13, rule: "Never involve lawyers." },
+  { number: 14, rule: "Bend the line, don’t break it." },
+  { number: 15, rule: "Always work as a team." },
+  { number: 16, rule: "If someone thinks he has the upper hand, break it." },
+  { number: 18, rule: "It’s better to seek forgiveness than ask permission." },
+  { number: 20, rule: "Always look under." },
+  { number: 22, rule: "Never, ever bother Gibbs in interrogation." },
+  { number: 23, rule: "Never mess with a Marine’s coffee if you want to live." },
+  {
+    number: 27,
+    rule: "Two ways to follow someone. First way, they never notice you. Second way, they only notice you.",
+  },
+  { number: 28, rule: "When you need help, ask." },
+  { number: 35, rule: "Always watch the watchers." },
+  { number: 36, rule: "If it feels like you’re being played, you probably are." },
+  { number: 38, rule: "Your case, you’re lead." },
+  { number: 39, rule: "There is no such thing as a coincidence." },
+  { number: 40, rule: "If it seems like someone’s out to get you, they are." },
+  { number: 42, rule: "Never accept an apology from somebody who just sucker punched you." },
+  { number: 44, rule: "First things first, hide the women and children." },
+  { number: 45, rule: "Left a mess I gotta clean up." },
+  { number: 51, rule: "Sometimes you’re wrong." },
+  { number: 62, rule: "Always give people space when they get off an elevator." },
+  { number: 69, rule: "Never trust a woman who doesn’t trust her man." },
+  { number: 91, rule: "When you decide to walk away, never look back." },
+];
