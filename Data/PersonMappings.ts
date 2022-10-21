f2f6c0b68afdb83dae596f6dd453f40aad38512d
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-21 19:23:29 +0100

    Daily quotes

---
+++ Data/PersonMappings.ts
@@ -0,0 +1,7 @@
+import { Person } from "../Components/Quote";
+
+export const PersonMappings: { person: Person; imagePath: string }[] = [
+  { person: "Gibbs", imagePath: "" },
+  { person: "Ziva", imagePath: "" },
+  { person: "Tony", imagePath: "" },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import GibbsRule from "../Components/GibbsRule";
+import ZivaMisquote from "../Components/ZivaMisquote";
+import { gibbsRules } from "../Data/GibbsRules";
+import { tonyJokes } from "../Data/TonyJokes";
+import { zivaMisquotes } from "../Data/ZivaMisquotes";
+import { getDeterministicArrayItems } from "../Helpers/DeterministicSeeding";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const dailyGibbsRule = getDeterministicArrayItems(1, gibbsRules)[0];
+  const dailyZivaMisquote = getDeterministicArrayItems(1, zivaMisquotes)[0];
+  //const dailyTonyJoke = getDeterministicArrayItems(1, tonyJokes)[0];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +21,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <GibbsRule ruleInfo={dailyGibbsRule}/>
+      <ZivaMisquote misquoteInfo={dailyZivaMisquote}/>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
