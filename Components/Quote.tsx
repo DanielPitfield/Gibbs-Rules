4c7fdf58b8dd22477bfec07ef4d91ee27eb1e60d
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-24 00:58:14 +0100

    Quote prop - imageIndex

--- Components/Quote.tsx
+++ Components/Quote.tsx
@@ -1,5 +1,5 @@
 import { Person, PersonMappings } from "../Data/PersonMappings";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 export type QuoteTemplate = { person: Person; quote: string };
 
@@ -7,19 +7,40 @@ interface QuoteProps {
   person: Person;
   title?: string;
   flair?: "emergency" | "golden";
+  imageIndex?: number;
   showImage: boolean;
   message: string;
 }
 
 const Quote = (props: QuoteProps) => {
-  const image = PersonMappings.find((mapping) => props.person === mapping.person)?.image;
+  const getImage = (): StaticImageData | null => {
+    if (!props.showImage) {
+      return null;
+    }
+
+    const imageArray = PersonMappings.find((mapping) => props.person === mapping.person)?.images;
+
+    if (!imageArray) {
+      return null;
+    }
+
+    // props.imageIndex is a valid index
+    if (props.imageIndex && props.imageIndex >= 0 && props.imageIndex <= imageArray.length - 1) {
+      return imageArray[props.imageIndex];
+    }
+
+    // A randomly selected image
+    return imageArray[Math.floor(Math.random() * imageArray.length)];
+  };
+
+  const image = getImage();
 
   return (
     <div className="quote-wrapper" data-flair={props.flair}>
       <div className="quote-container">
         {props.title && <div className="quote-title">{props.title}</div>}
         <div className="quote-image">
-          {props.showImage && image ? (
+          {image ? (
             <Image src={image} alt={props.person} width={550} height={270} layout="fixed" />
           ) : (
             <span>{props.person}</span>
--- Data/PersonMappings.ts
+++ Data/PersonMappings.ts
@@ -6,9 +6,9 @@ import McgeeImg from "./images/mcgee.jpg";
 
 export type Person = "Gibbs" | "Ziva" | "Tony" | "McGee";
 
-export const PersonMappings: { person: Person; image: StaticImageData }[] = [
-  { person: "Gibbs", image: GibbsImg },
-  { person: "Ziva", image: ZivaImg },
-  { person: "Tony", image: TonyImg },
-  { person: "McGee", image: McgeeImg },
+export const PersonMappings: { person: Person; images: StaticImageData[] }[] = [
+  { person: "Gibbs", images: [GibbsImg] },
+  { person: "Ziva", images: [ZivaImg] },
+  { person: "Tony", images: [TonyImg] },
+  { person: "McGee", images: [McgeeImg] },
 ];
