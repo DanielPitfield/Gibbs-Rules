94802f94c7ff760b771dffbb1af18ef779780ddd
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-23 04:44:37 +0100

    NavBar pageLinks

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -2,21 +2,25 @@ import Link from "next/link";
 import React from "react";
 
 export const NavBar = () => {
+  const pageLinks: { name: string; path: string }[] = [
+    { name: "Home", path: "/" },
+    { name: "Gibbs", path: "/Gibbs" },
+  ];
+
   return (
     <header>
       <h1>Gibbs Rules</h1>
 
       <ul className="navbar-links-wrapper">
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/Gibbs">
-            <a>Gibbs</a>
-          </Link>
-        </li>
+        {pageLinks.map((pageLink) => {
+          return (
+            <li key={pageLink.name}>
+              <Link href={pageLink.path}>
+                <a>{pageLink.name}</a>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </header>
   );
