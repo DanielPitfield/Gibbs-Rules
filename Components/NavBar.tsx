6e4b69f377c878788f4b0d896b97d5ed71719839
Benj <benj>
2022-10-24 20:40:53 +0100

    Add date to header

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export const NavBar = () => {
   return (
     <header>
-      <h1>Gibbs Rules</h1>
+      <h1>NCIS Daily Quotes</h1>
+      <h2>{new Date().toLocaleDateString("en-GB", { year: "numeric", month: "long", day: "2-digit"})}</h2>
     </header>
   );
 };
