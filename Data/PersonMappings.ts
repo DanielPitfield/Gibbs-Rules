cf7cc7bea143532d9e383d301c912bf97d44bbde
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-24 03:59:40 +0100

    Refactor daily quotes

--- Data/PersonMappings.ts
+++ Data/PersonMappings.ts
@@ -3,6 +3,10 @@ import GibbsImg from "./images/gibbs.jpg";
 import ZivaImg from "./images/ziva.jpg";
 import TonyImg from "./images/tony.jpg";
 import McgeeImg from "./images/mcgee.jpg";
+import { ConversationTemplate } from "../Components/Conversation";
+import { gibbsRules } from "./QuoteArrays/GibbsRules";
+import { zivaMisquotes } from "./QuoteArrays/ZivaMisquotes";
+import { tonyJokes } from "./QuoteArrays/TonyJokes";
 
 export type Person = "Gibbs" | "Ziva" | "Tony" | "McGee";
 
@@ -22,9 +26,11 @@ export const McgeeImages = {
   DEFAULT: McgeeImg,
 };
 
-export const PersonMappings: { person: Person; images: StaticImageData[] }[] = [
-  { person: "Gibbs", images: Object.values(GibbsImages) },
-  { person: "Ziva", images: Object.values(ZivaImages) },
-  { person: "Tony", images: Object.values(TonyImages) },
-  { person: "McGee", images: Object.values(McgeeImages) },
+export const PersonMappings: { person: Person; images: StaticImageData[]; array: ConversationTemplate[] }[] = [
+  { person: "Gibbs", images: Object.values(GibbsImages), array: gibbsRules },
+  { person: "Ziva", images: Object.values(ZivaImages), array: zivaMisquotes },
+  { person: "Tony", images: Object.values(TonyImages), array: tonyJokes },
+  { person: "McGee", images: Object.values(McgeeImages), array: [] },
 ];
+
+export const DailyCharactersInfo = PersonMappings.filter((person) => person.array.length > 0);
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { NavBar } from "../Components/NavBar";
 import Conversation, { ConversationTemplate } from "../Components/Conversation";
-import { gibbsRules } from "../Data/QuoteArrays/GibbsRules";
-import { zivaMisquotes } from "../Data/QuoteArrays/ZivaMisquotes";
 import { getDeterministicArrayItems } from "../Helpers/DeterministicSeeding";
-import { tonyJokes } from "../Data/QuoteArrays/TonyJokes";
+import { DailyCharactersInfo } from "../Data/PersonMappings";
 
 /** Model of the properties of the component */
 type HomePageProps = {
-  dailyGibbsRule: ConversationTemplate;
-  dailyZivaMisquote: ConversationTemplate;
-  dailyTonyJoke: ConversationTemplate;
+  dailyQuotes: ConversationTemplate[];
 };
 
 /**
@@ -19,11 +15,10 @@ type HomePageProps = {
  * @returns Properties of the component.
  */
 export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
-  const dailyGibbsRule = getDeterministicArrayItems(1, gibbsRules)[0];
-  const dailyZivaMisquote = getDeterministicArrayItems(1, zivaMisquotes)[0];
-  const dailyTonyJoke = getDeterministicArrayItems(1, tonyJokes)[0];
+  // Get the daily quote for each character
+  const dailyQuotes = DailyCharactersInfo.map((person) => getDeterministicArrayItems(1, person.array)[0]);
 
-  return { props: { dailyGibbsRule, dailyZivaMisquote, dailyTonyJoke } };
+  return { props: { dailyQuotes: dailyQuotes } };
 };
 
 /**
@@ -43,9 +38,10 @@ const Home: NextPage<HomePageProps> = (props) => {
       <NavBar />
 
       <main>
-        <Conversation conversation={props.dailyGibbsRule} />
-        <Conversation conversation={props.dailyZivaMisquote} />
-        <Conversation conversation={props.dailyTonyJoke} />
+        {props.dailyQuotes.map((dailyQuote, index) => {
+          // Display the daily quote of each character
+          return <Conversation key={index} conversation={dailyQuote} />;
+        })}
       </main>
     </div>
   );
