c10a6830c17c0fffd7ae060cb9ad0288db3c33f1
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-26 23:13:32 +0100

    Revert "Horizontal styling"
    This reverts commit 6fa83b0660de952031277e91496b83507d94df0e.

--- Data/QuoteArrays/TonyJokes.ts
+++ Data/QuoteArrays/TonyJokes.ts
@@ -5,6 +5,4 @@ export const tonyJokes: ConversationTemplate[] = [
     { person: "McGee", message: "I've got it! Now my CPU's too small!" },
     { person: "Tony", message: "I'm gonna let that slide" },
   ],
-  [{ person: "Tony", message: "My name is Anthony. Friends call me Tony, which backwards is 'Y Not'" }],
-  [{ person: "Tony", message: "Big 'D', little 'i', Big 'N', little 'ozzo'" }],
 ];
