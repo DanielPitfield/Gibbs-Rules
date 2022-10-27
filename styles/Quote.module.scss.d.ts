7c2176b8027142a4543fc776ace0cfaad4d22883
Benj <benj>
2022-10-27 19:23:05 +0100

    Remove typed SCSS modules

--- styles/Quote.module.scss.d.ts
+++
@@ -1,6 +0,0 @@
-export const container: string;
-export const image: string;
-export const message: string;
-export const shimmer: string;
-export const title: string;
-export const wrapper: string;
