bc8853862567f104b0d64f4bb7e95fd6f5176af4
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-27 02:21:44 +0100

    Typed SCSS Modules

---
+++ styles/Quote.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const container: string;
+export const image: string;
+export const message: string;
+export const shimmer: string;
+export const title: string;
+export const wrapper: string;
