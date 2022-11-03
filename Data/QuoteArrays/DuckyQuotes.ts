42e1c09093f00901a40d28b2d73e349e3ded7430
Benj <benj>
2022-11-03 19:17:51 +0000

    'iconic' flair

--- Data/QuoteArrays/DuckyQuotes.ts
+++ Data/QuoteArrays/DuckyQuotes.ts
@@ -3,6 +3,6 @@ import { ConversationTemplate } from "../../app/Conversation";
 export const duckyQuotes: ConversationTemplate[] = [
   [
     { person: "Bodnar", message: "And whose grandfather would you be?" },
-    { person: "Ducky", message: "Well that depends. Who's your grandmother?" },
+    { person: "Ducky", message: "Well that depends. Who's your grandmother?", flair: "iconic" },
   ],
 ];
