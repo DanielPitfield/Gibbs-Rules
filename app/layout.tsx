20a793fc11df3fe8a3afebde6240ea0c95564b1a
DanielPitfield <danielpitfield1@gmail.com>
2023-06-07 11:37:08 +0100

    Remove date from title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import "../public/styles/index.scss";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const formattedDate = new Date().toLocaleDateString("en-GB", { year: "numeric", month: "long", day: "2-digit" });
-
   return (
     <html lang="en">
       <head>
-        <title>{`Daily Quotes | ${formattedDate}`}</title>
+        <title>Daily Quotes</title>
         <meta name="description" content="Daily quotes" />
         <link rel="icon" href="/favicon.ico" />
       </head>
 
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
